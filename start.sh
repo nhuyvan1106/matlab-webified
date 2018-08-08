d18651c25f6786a51fe9cc65ce94922317e7089b
Van <vann363@we25587.pnl.gov>
2018-08-08 15:40:44 -0700

    - Update build script to load all jar files inside jars folder and start script for windows

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 ./gradlew war
 if [ $? -eq 0 ]
 then
-    java -jar payara-micro-5.182.jar --port 8080  --deploy build/libs/matlab-webified-1.0.0.war --addlibs /Applications/MATLAB/MATLAB_Runtime/v91/toolbox/javabuilder/jar/javabuilder.jar
+    java -jar payara-micro-5.182.jar --port 8080  --deploy build/libs/matlab-webified-1.0.0.war --addlibs "/Applications/MATLAB/MATLAB_Runtime/v91/toolbox/javabuilder/jar/javabuilder.jar" 
 fi
